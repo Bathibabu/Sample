b70c41d508ee5d2613149e5a65c69d98e31857dc
Sid <bathibabu.doddi67@gmail.com>
2022-03-02 16:41:52 +0530

    initial commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Bathi on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
